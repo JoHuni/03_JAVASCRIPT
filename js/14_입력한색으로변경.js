1defd1804a056f4f794040894fb56586d5a0fdc5
DESKTOP-T7J1UF5\guppyOwO <hhcho1234@naver.com>
2024-01-18 20:41:16 +0900

    찐최종

--- js/14_입력한색으로변경.js
+++ js/14_입력한색으로변경.js
@@ -4,7 +4,17 @@ const btn = document.querySelector('#btn');
 
 btn.addEventListener("click", function(){
     for (let i = 0; i < input3.length; i++) {
-        const color = input3[i].value;
-        box3[i].style.backgroundColor = color;
+        box3[i].style.backgroundColor = input3[i].value;
     }
 });
+
+
+for (let i = 0; i < input3.length; i++) {
+    input3[i].addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            box3[i].style.backgroundColor = input3[i].value;
+        }
+    });
+}
+
+
