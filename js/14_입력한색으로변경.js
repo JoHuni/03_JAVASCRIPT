a1718bd30f6b739b21e6c9812f01717ee2d9959b
DESKTOP-T7J1UF5\guppyOwO <hhcho1234@naver.com>
2024-01-18 19:47:59 +0900

    d

--- js/14_입력한색으로변경.js
+++ js/14_입력한색으로변경.js
@@ -3,6 +3,9 @@ const input3 = document.querySelector('#input3');
 const btn = document.querySelector('#btn');
 
 input3.addEventListener("keydown", function(e){
+    for(let i = 0; i < box3.length; i++){
+        box3[i];
+    }
     if(e.key == 'Enter'){
         box3.style.backgroundColor = input3.value
     }
